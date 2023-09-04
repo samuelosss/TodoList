376fea70b23ba67e654157adf7065600269065b5
Sam Poloha <samuel.poloha@gmail.com>
2023-09-04 13:07:01 +0200

    collapsing lists, some styling

--- public/script/actions.js
+++ public/script/actions.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 	$('input[type="checkbox"]').on("click", function (e) {
-		// Toggle the "striked" class of the clicked LI
+		// Toggle the "striked" class of the clicked LI when checkbox checked
 		$(this).parent().toggleClass("striked");
 		e.stopPropagation(); // Prevent the LI click event from triggering
 	});
 
-	// Add a click event handler to the LI elements, checkboxes, and labels
+	// Add a click event handler to the LI elements
 	$("li").on("click", function (e) {
 		// Toggle the "striked" class of the clicked LI
 		$(this).toggleClass("striked");
@@ -16,6 +16,7 @@ $(document).ready(function () {
 		checkbox.prop("checked", !checkbox.prop("checked"));
 	});
 
+	// Add a click event handler to the label elements
 	$("label").on("click", function (e) {
 		// Toggle the "striked" class of the clicked label
 		$(this).toggleClass("striked");
@@ -23,5 +24,13 @@ $(document).ready(function () {
 		const checkbox = $(this).parent().find('input[type="checkbox"]');
 		// Toggle the checkbox state (checked/unchecked)
 		checkbox.prop("checked", !checkbox.prop("checked"));
+	});
+
+	// Collapsing lists clicking on the title
+	$(".list-title").on("click", function (collapse) {
+		console.log("clicked!");
+		$(this).next(".padd").slideToggle({
+			duration: 1000,
+		});
 	});
 });
